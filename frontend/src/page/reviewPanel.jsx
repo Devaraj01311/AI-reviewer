e96021687cd1cad1c4b181b0a593e7cae5970d7c
Devaraj L <devarajldev01@gmail.com>
2025-09-10 17:14:56 +0530

    add..

--- frontend/src/page/reviewPanel.jsx
+++ frontend/src/page/reviewPanel.jsx
@@ -3,7 +3,7 @@ import axios  from "axios";
 function ReviewPanel({code, review ,setReview}){
      async function reviewCode() {
     try {
-      const response = await axios.post('https://ai-code-reviewer-fg5m.onrender.com/ai/get-review', { code })
+      const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/ai/get-review`, { code })
       console.log("Backend Response:", response.data)
       setReview(response.data.review)  
     } catch (err) {
